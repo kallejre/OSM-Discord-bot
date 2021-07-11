dd18581e84e7155885a65208a3286440284d1fc8
kallejre <kallejre@yahoo.com>
2021-07-11 13:23:49 +0300

    Changes related to config management
    Biggest new thing is that config handling is now in a separate file and to read config, all you need now is `from configuration import config`.
    Many minor changes related to moving constants from main to config.
    Remember, this branch is pretty much copy-paste of older commits on main branch.

---
+++ configuration.py
@@ -0,0 +1,31 @@
+# /bin/python3
+# Configuration loader
+import json
+import math
+from typing import Any
+
+
+def load_config() -> None:
+    global config, guild_ids
+    # LINK - config.json
+    with open("config.json", "r", encoding="utf8") as file:
+        config = json.loads(file.read())
+    guild_ids = [int(x) for x in config["server_settings"].keys()]
+
+
+def save_config() -> None:
+    global config
+    # LINK - config.json
+    with open("config.json", "w", encoding="utf8") as file:
+        file.write(json.dumps(config, indent=4))
+
+
+# SyntaxError: annotated name 'config' can't be global
+config = dict()  # : dict[str, Any] = {}
+guild_ids = list()  # : list[int] = []
+load_config()
+
+### Rate-limiting ###
+exp = math.log(config["rate_limit"]["max_calls"], config["rate_limit"]["max_elements"])
+config["rate_limit"]["element_count_exp"] = round(exp, 2)  # 1.17
+# Element count exponent is used by quota calculation when inline command wants to solme many elements.
